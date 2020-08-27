7850bf9672a4e404b6fbe543a66cd61d177679d4
imyuanandyou <294174098@qq.com>
2020-08-27 17:59:44 +0800

    first commit

---
+++ src/test/java/com/yuan/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
